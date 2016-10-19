e6b1a7f8fee9b71a3c41764eb35d6fdd6c377426
Kúr Attila <kur.attila+github@gmail.com>
2016-10-19 21:59:02 +0200

    Windows deployment script: with configurable paths

--- WinDeploy - MSVC2015.bat
+++ WinDeploy - MSVC2015.bat
@@ -1,12 +1,20 @@
-set path=%path%;d:\Qt\Qt5.6\5.6\msvc2015\bin\
-windeployqt --qmldir src\TombolaApp d:\git-repos\build-Tombola-Desktop_Qt_5_6_0-Release\src\TombolaApp\release\TombolaApp.exe
+rem ============================================
+rem Customize these paths to your own locations:
+rem ============================================
+set QtBinPath=D:\Qt\Qt5.6\5.6\msvc2015\bin
+set ReleaseBuildDir=D:\git-repos\build-Tombola-Desktop_Qt_5_6_0-Release
+
+
+
+set path=%path%;%QtBinPath%
+windeployqt --qmldir src\TombolaApp %ReleaseBuildDir%\src\TombolaApp\release\TombolaApp.exe
 
 rem Copy over .DLL file and also its .PDB file
-copy /Y d:\git-repos\build-Tombola-Desktop_Qt_5_6_0-Release\src\TombolaLib\release\TombolaLib.dll d:\git-repos\build-Tombola-Desktop_Qt_5_6_0-Release\src\TombolaApp\release\
-copy /Y d:\git-repos\build-Tombola-Desktop_Qt_5_6_0-Release\src\TombolaLib\release\TombolaLib.pdb d:\git-repos\build-Tombola-Desktop_Qt_5_6_0-Release\src\TombolaApp\release\
+copy /Y %ReleaseBuildDir%\src\TombolaLib\release\TombolaLib.dll %ReleaseBuildDir%\src\TombolaApp\release\
+copy /Y %ReleaseBuildDir%\src\TombolaLib\release\TombolaLib.pdb %ReleaseBuildDir%\src\TombolaApp\release\
 
 remo Copy over localization *.qm files
-copy /Y d:\git-repos\build-Tombola-Desktop_Qt_5_6_0-Release\src\TombolaApp\*.qm d:\git-repos\build-Tombola-Desktop_Qt_5_6_0-Release\src\TombolaApp\release\
+copy /Y %ReleaseBuildDir%\src\TombolaApp\*.qm %ReleaseBuildDir%\src\TombolaApp\release\
 
 rem strnlen missing warning on XP
 rem https://bugreports.qt.io/browse/QTBUG-44985
